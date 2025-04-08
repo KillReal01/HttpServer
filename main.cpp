08ca97d6cc25a35f940997daf9287bb4c0032fe1
kbereza <kbereza@linux.linux.virtualbox.org>
2025-04-08 20:20:20 +0300

    append upload handler

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 
 
 extern "C" {
@@ -25,9 +26,30 @@ void HandleLog(mg_connection* c, mg_http_message* hm)
 }
 
 
-void HandleUpload(mg_connection* c, mg_http_message* hm)
+void HandleUpload(mg_connection* conn, mg_http_message* hm)
 {
-    mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "Return upload");
+    mg_http_part part;
+    size_t pos = 0;
+
+    while ((pos = mg_http_next_multipart(hm->body, pos, &part)) > 0)
+    {
+        std::string filename(part.filename.buf, part.filename.len);
+        std::string filepath = "/tmp/" + filename;
+        std::string filedata(part.body.buf, part.body.len);
+
+        std::ofstream out(filepath, std::ios::binary);
+        if (out.is_open())
+        {
+            out.write(filedata.data(), filedata.size());
+            out.close();
+            spdlog::info("Saved upload to {}", filepath);
+            mg_http_reply(conn, 200, "", "Uploaded to %s\n", filepath.c_str());
+        } 
+        else
+        {
+            mg_http_reply(conn, 500, "", "Failed to save file\n");
+        }
+    }
 }
 
 
